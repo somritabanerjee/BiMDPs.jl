4c9d4dd6b5bb5c54b73a95a357113eafddd971c6
Somrita Banerjee <sbanerj6@ndc.nasa.gov>
2023-09-23 02:46:44 -0700

    Added a case dictionary

--- src/BiMDPs.jl
+++ src/BiMDPs.jl
@@ -12,4 +12,23 @@ include("RoverWorld/RoverWorld.jl")
 export
     RoverWorld
 
+case_dictionary = Dict("case001" => RoverWorld.RoverWorldMDP(
+                                            grid_size = (10,10),
+                                            max_time = 20,
+                                            tgts = Dict(1=>((9,2),(1,18),50),
+                                                        2=>((9,8),(15,20),50)),
+                                            obstacles = [],
+                                            exit_xys = []
+                        ),
+                        "case002" => RoverWorld.RoverWorldMDP(
+                                            grid_size = (10,10),
+                                            max_time = 20,
+                                            tgts = Dict(1=>((9,2),(1,18),50),
+                                                        2=>((9,8),(15,20),50)),
+                                            obstacles = [((6,6), (1,20), -5)],
+                                            exit_xys = []
+                        )
+)
+export case_dictionary            
+
 end # module
